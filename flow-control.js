aa48d9dce294d70e5f76c91feac8aa6e840ed1ef
Blake A Long <blake.a.long@gmail.com>
2019-01-30 04:17:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,55 +1,48 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   }
 }
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else {
-    return "You are not a teenager"
+    return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else if (age < 13) {
-    return "You are a kid"
+    return "You are a kid";
   } else {
-    return "You are a grownup"
+    return "You are a grownup";
   }
 }
 
 function ternaryTeenager(age) {
-  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager"
+  return age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager";
 }
 
 function switchAge(age) {
   switch(age) {
     case "13":
-      "You are a teenager";
-      break;
+      return "You are a teenager";
     case "14":
-      "You are a teenager";
-      break;
+      return "You are a teenager";
     case "15":
-       "You are a teenager";
-       break;
+       return "You are a teenager";
     case "16":
-       "You are a teenager";
-       break;
+       return "You are a teenager";
     case "17":
-       "You are a teenager";
-       break;
+       return "You are a teenager";
     case "18":
-       "You are a teenager";
-       break;
+       return "You are a teenager";
     case "19":
-       "You are a teenager";
-       break;
+       return "You are a teenager";
     default:
-       "You have an age";
+       return "You have an age";
   }
 }
